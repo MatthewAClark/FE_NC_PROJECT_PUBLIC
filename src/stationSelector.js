d3b541549690e23aad8d59eedffce339088b3880
MatthewAClark <matthewclark86@gmail.com>
2018-07-16 21:00:55 +0100

    working on delete function on stations

--- src/stationSelector.js
+++ src/stationSelector.js
@@ -6,101 +6,145 @@ import NewStation from "./newStation"
 class StationTable extends Component {
 
     // Stations list stored in state
-  state = {
-    stations: [{station_name: 'test'},{station_name: 'test2'}],
-    editClicked: false
-  }
+    state = {
+        station_name: '',
+        stations: [],
+        editClicked: false
+    }
 
-  // functions
-  deleteStation = (i) => {
-    const deleteStation = this.state.stations.filter((elem, index) => {
-      return (index !== i)
-    })
+    componentDidMount() {
 
-    this.setState({
-      stations: deleteStation
-    })
-  }
+        //Display all stations from db on first load
+        let fetchUrl;
 
-  createStation = (event) => {
-    event.preventDefault();
-    const newStations = this.state.stations.concat([{ station_name: this.state.station_name }])
-    this.setState({
-      stations: newStations
-    })
-  }
+        fetchUrl = "http://localhost:3000/api/db/stations/"
 
-  // 
-  handleStationName = (event) => {
-    const station_name = event.target.value
-    this.setState({
-      station_name
-    })
+        fetch(fetchUrl)
 
-  }
+            .then(res => {
+                return res.json();
+            })
+            .then(body => {
+                this.setState({ stations: body })
 
-  // editHeader for Station component
-  editHeader = (text, index) => {
-    const { stations } = this.state
-    const newStations = [...stations]
-    newStations[index].stationName = text
-    this.setState({
-      lists: newStations
-    }, () => {
-      console.log('state updated')
-    })
-  }
+            })
+    }
+    // functions
+    deleteStation = (i) => {
 
-  // Toggle button for stations
-  toggleButton = (event) => {
-    this.setState({
-      buttonClicked: !this.state.buttonClicked
-    })
-  }
+ 
+                const deleteStation = this.state.stations.filter((elem, index) => {
+                    return (index !== i)
+                })
 
-  // Edit button for lists
-  editButton = (event) => {
-    this.setState({
-      editClicked: !this.state.editClicked
-    })
-  }
+                // const deleteStationDb = this.state.stations.find((elem, index) => {
+                //     return (index === 1)
+                // })
+                // console.log(deleteStationDb)
+                // fetch(`http://localhost:3000/api/db/stations/${deleteStationDb.station_id}`, {
+                //     method: 'DELETE'
+                // })
+                //     .then(res => {
+                this.setState({
+                    stations: deleteStation
+                })
+            // })
+    }
 
-  // render code
-  render() {
+    createStation = (event) => {
+        event.preventDefault();
 
-    return ( // a list
-      <div className="column is-narrow listItem">
+        // post new station to db
+        fetch(`http://localhost:3000/api/db/stations/`, {
+            headers: new Headers({ "Content-Type": "application/json" }),
+            method: 'POST',
+            body: JSON.stringify({
+                station_name: this.state.station_name
+            })
+        })
+            .then(res => {
+                if (res.status === 201) {
+                    const newStations = this.state.stations.concat([{ station_name: this.state.station_name }])
+                    this.setState({
+                        stations: newStations
+                    })
+                }
+            })
 
-        {/* {// Header of list
+
+    }
+
+    // 
+    handleStationName = (event) => {
+        const station_name = event.target.value
+        this.setState({
+            station_name
+        })
+
+    }
+
+    // editHeader for Station component
+    editHeader = (text, index) => {
+        const { stations } = this.state
+        const newStations = [...stations]
+        newStations[index].stationName = text
+        this.setState({
+            lists: newStations
+        }, () => {
+            console.log('state updated')
+        })
+    }
+
+    // Toggle button for stations
+    toggleButton = (event) => {
+        this.setState({
+            buttonClicked: !this.state.buttonClicked
+        })
+    }
+
+    // Edit button for lists
+    editButton = (event) => {
+        this.setState({
+            editClicked: !this.state.editClicked
+        })
+    }
+
+    // render code
+    render() {
+
+        return ( // a list
+            <div className="column is-narrow listItem">
+
+                {/* {// Header of list
         }
         <ListHeader editButton={this.editButton} editClicked={this.state.editClicked} listName={this.props.listName} editHeader={this.props.editHeader} index={this.props.index} /> */}
 
-        {// Station items on the list
-        }
+                {// Station items on the list
+                }
 
-        {/* Display current stations on table */}
-        <div className="stationItem">
-          {this.state.stations.map((station, i) => {
+                {/* Display current stations on table */}
+                <div className="stationItem">
+                    {this.state.stations.map((station, i) => {
 
-            return (<Station index={i} station_name={station.station_name} deleteStation={this.deleteStation} editHeader={this.editHeader} />)
-          })}
-
-
-          {/* Display new station on the list*/}
-          <div>
-            <NewStation handleStationName={this.handleStationName} createStation={this.createStation} buttonClicked={this.state.buttonClicked} toggleButton={this.toggleButton} />
-
-          </div>
+                        return (<Station index={i} station_name={station.station_name} deleteStation={this.deleteStation} editHeader={this.editHeader} />)
+                    })}
 
 
+                    {/* Display new station on the list*/}
+                    <div>
+                        <NewStation handleStationName={this.handleStationName} createStation={this.createStation} buttonClicked={this.state.buttonClicked} toggleButton={this.toggleButton} />
 
-          
+                    </div>
 
 
-        </div>
-      </div>
-    );
-  }
+
+
+
+
+                </div>
+            </div>
+        );
+    }
 }
 
 export default StationTable;
